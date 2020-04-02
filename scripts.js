bfbaf0e98e1e934ce0ca0df3c11a9db90a18c528
Suman Lama <sumnjlama@gmail.com>
2020-04-02 21:16:46 +0100

    latest1

---
+++ scripts.js
@@ -0,0 +1,8 @@
+/*------gallery -------------*/
+
+/*var galleryImg = document.getElementsByClassName(gallery_img);*/
+
+function enlargePhotos() {
+    var galleryImg = document.getElementsByClassName("gallery_img");
+    galleryImg.style.display = 'none';
+};
