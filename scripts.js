7857046904248aa7b5ca3e7cfbebcfc97250f380
Suman Lama <sumnjlama@gmail.com>
2020-04-08 15:00:35 +0100

    newLy

--- scripts.js
+++ scripts.js
@@ -1,8 +1,36 @@
+/*-----------------------------------Index slideshow-----------------------------------------
+
+var autImgSlide = 0;
+img_slide();
+
+function img_slide() {
+    var x = document.getElementsByClassName("indexImg1");
+
+    for (var i = 0; i > x.length; i++) {
+        x[i].style.display = "none";
+    }
+
+    autImgSlide++;
+    if (autImgSlide > x.length) {
+        autImgSlide = 1
+    }
+    
+    x[autImgSlide - 1].style.display = " ";
+    setTimeout(img_slide, 2000);
+}*/
+
+
+
+
+
+
+
+
 /*------gallery -------------*/
 
-/*var galleryImg = document.getElementsByClassName(gallery_img);*/
+/*var galleryImg = document.getElementsByClassName(gallery_img);
 
 function enlargePhotos() {
     var galleryImg = document.getElementsByClassName("gallery_img");
     galleryImg.style.display = 'none';
-};
+};*/
