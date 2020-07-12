c152717251764e3ec005a10769a6e5376905ea31
Suman Lama <sumnjlama@gmail.com>
2020-07-12 15:49:55 +0100

    12/07/20 - 1

--- scripts.js
+++ scripts.js
@@ -35,17 +35,65 @@ let hideClsBtn = () => {
         menuCloseButton.style.display = "block";
     }, 250);
 }
+/*let expandImage = (imgs) => { 
+    
+    document.getElementById('expand-images').style.display = 'flex';//display of the active image
+    let zoomPic = document.getElementById('expandable');//class of the active image div
+    zoomPic.src = imgs.src;//active pic src = selected pic src
 
+    let hideTopSect = () => {//hides the top section
+        document.getElementById("big-pics").style.display = 'none';
+    };
+    hideTopSect();
+}*/
 let expandImage = (imgs) => {
-    document.getElementById('expand-images').style.display = 'flex';
-    let zoomPic = document.getElementById('expandable');
-    zoomPic.src = imgs.src;
-
-    let hideTopSect = () => {
+    document.getElementById('expand-images').style.display = 'flex'; //display of the active image
+    let zoomPic = document.getElementById('expandable'); //class of the active image div
+    zoomPic.src = imgs.src; //active pic src = selected pic src
+    
+    let hideTopSect = () => { //hides the top section
         document.getElementById("big-pics").style.display = 'none';
     };
     hideTopSect();
 }
+
+let nextImg = (n) => {
+    let imags = document.getElementsByClassName("gallery--img"); //images class name
+    let zoomedPic = document.getElementById('expandable'); //highlighted image
+    let zoomedPicSrc = zoomedPic.src;
+    for (var i = 0; i < imags.length; i++) { //loop through all the image elements
+        if (zoomedPicSrc === imags[i].src) { //stop at the src of the present imaage
+            i += n; //add or minus the value of argument (n)
+            zoomedPic.src = imags[i].src; //change highlighted images src
+            //console.log(zoomedPicSrc);
+            return zoomedPicSrc; //end loop
+        }
+    }
+}
+
+
+
+/*let slideImg = (n) => {
+    var i; //for loop var declaration
+    var imgs = document.getElementsByClassName("gallery--img"); //images class name
+    if (n > imgs.length) {
+        slideIndex = 1
+    }; //if imgs ends it goes back to the first image
+    if (n < 1) {
+        slideIndex = imgs.length
+    }; //goes to last image if clicked previous on the 1st image
+
+    for (i = 0; i < imgs.length; i++) { //loops through all the images to hide its display
+        imgs[i].style.display = "none";
+    }
+    imgs[slideIndex - 1].style.display = "flex"; //display the selected image
+
+}
+slideImg(slideIndex);
+var slideIndex = 1; //default index number
+let nextImg = (n) => slideImg(slideIndex += n);*/
+
+
 
 let showTopSect = () => {
     document.getElementById("big-pics").style.display = 'grid';
@@ -59,7 +107,7 @@ function scrollIt() {
 
             if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
                 navBar.style.height = "8vh";
-                
+
 
                 document.getElementById("nav-list").style.top = "0";
             } else {
@@ -68,13 +116,12 @@ function scrollIt() {
                 document.getElementById("nav-list").style.top = "5px";
 
             }
-            
-        }; 
-        
+
+        };
+
     };
 };
 scrollIt();
-
 
 
 /*let slideMenuHeight = () => {
